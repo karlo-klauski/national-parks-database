362e54a1d2be32dc757de84de58d7a80672c83b2
Karlo Klauški <karlo.klauski@tutamail.com>
2025-12-27 10:19:12 +0100

    add openapi.json and lab3.routes.js

---
+++ src/routes/lab3.routes.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const db = require('../db/db');
+const { Parser } = require('json2csv');
+
+const router = express.Router();
+
+router.get('data', async (req, res) => {
+    
+});
