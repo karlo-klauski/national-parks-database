b4066520040284f26935672bc2c94bb4a91ba134
Karlo Klauški <karlo.klauski@tutamail.com>
2026-01-14 10:20:54 +0100

    enable auth service

--- app.js
+++ app.js
@@ -3,15 +3,29 @@ const path = require('path');
 const fs = require('fs')  // file system
 const bodyParser = require('body-parser');
 const db = require('./src/db/db');
+const { auth } = require('express-openid-connect');
+const { requiresAuth } = require('express-openid-connect');
 
 const indexRoutes = require('./src/routes/index.routes.js');
 const datatableRoutes = require('./src/routes/datatable.routes.js');
 const APIroutes = require('./src/routes/api.routes.js');
 
+const config = {
+  authRequired: false,
+  auth0Logout: true,
+  secret: '9f8d7a6c5b4a3e2d1f0e9d8c7b6a5f4e',
+  baseURL: 'http://localhost:5000',
+  clientID: 'VS3l9ANBnUDtVONkLgamy963mqjUemrx',
+  issuerBaseURL: 'https://national-parks.eu.auth0.com'
+};
+
 const app = express();
 
 app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, 'src/views'));  // source folder for views
+
+// auth router attaches /login, /logout, and /callback routes to the baseURL
+app.use(auth(config));
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('public'));
@@ -21,9 +35,15 @@ app.use('/index', indexRoutes);
 app.use('/datatable', datatableRoutes);
 app.use('/api', APIroutes);
 
-app.get('/', (req, res) => { 
-    res.redirect('index');
-})
+// req.isAuthenticated is provided from the auth router
+app.get('/', (req, res) => {
+  res.send(req.oidc.isAuthenticated() ? 'Logged in' : 'Logged out');
+});
+
+app.get('/profile', requiresAuth(), (req, res) => {
+  res.send(JSON.stringify(req.oidc.user));
+});
+
 
 app.use((req, res) => {
     res.status(404).json({
